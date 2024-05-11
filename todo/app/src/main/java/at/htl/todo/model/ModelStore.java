7aeb40c3ca5c6981e598e767d5b938e888629376
aichingert <aichinger.tobias13@gmail.com>
2024-05-11 22:25:36 +0200

    feat: finished task 1

---
+++ todo/app/src/main/java/at/htl/todo/model/ModelStore.java
@@ -0,0 +1,26 @@
+package at.htl.todo.model;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/** This is our Storage area for <a href="https://redux.js.org/understanding/thinking-in-redux/three-principles">single source of truth</a> {@link Model}. */
+import at.htl.todo.util.store.Store;
+
+@Singleton
+public class ModelStore extends Store<Model>  {
+
+    @Inject
+    ModelStore() {
+        super(Model.class, new Model());
+    }
+
+    public void setTodos(Todo[] todos) {
+        Log.e("OMG", Integer.toString(todos.length));
+        for (Todo todo : todos) {
+            Log.e("OMG", todo.toString());
+        }
+        apply(model -> model.todos = todos);
+    }
+}
---
+++ todo/app/src/main/java/at/htl/todo/model/TodoService.java
@@ -0,0 +1,34 @@
+package at.htl.todo.model;
+
+
+import android.util.Log;
+
+import java.util.concurrent.CompletableFuture;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import at.htl.todo.util.resteasy.RestApiClientBuilder;
+
+@Singleton
+public class TodoService {
+    static final String TAG = TodoService.class.getSimpleName();
+    public static String JSON_PLACEHOLDER_BASE_URL = "https://jsonplaceholder.typicode.com";
+    public final TodoClient todoClient;
+    @Inject
+    ModelStore store;
+
+    @Inject
+    TodoService(RestApiClientBuilder builder, ModelStore store) {
+        Log.i(TAG, "Creating TodoService with base url: " + JSON_PLACEHOLDER_BASE_URL);
+        todoClient = builder.build(TodoClient.class, JSON_PLACEHOLDER_BASE_URL);
+        this.store = store;
+    }
+    public void getAll() {
+        Log.i("OMG", "THIS IS BS");
+        CompletableFuture
+                .supplyAsync(todoClient::all)
+                .thenAccept(store::setTodos);
+    }
+}
+
